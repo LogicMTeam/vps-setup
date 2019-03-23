035e3d1f0a5a592e5339dacd861d710093941f26
Cao Van Thanh <caovanthanh203@gmail.com>
2019-03-24 01:51:34 +0700

    add content

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 sudo -i
 yum update -y
-yum install zsh -y
+yum install nano zsh -y
 chsh -s /bin/zsh root
 echo $SHELL
 exit
@@ -24,3 +24,45 @@ cp -r /etc/skel/.oh-my-zsh ~/.oh-my-zsh
 cp /usr/share/.zshrc ~/.zshrc
 chsh -s /bin/zsh
 zsh
+
+yum install epel-release -y
+rpm -Uvh http://rpms.famillecollet.com/enterprise/remi-release-7.rpm
+rpm -Uvh http://nginx.org/packages/centos/7/noarch/RPMS/nginx-release-centos-7-0.el7.ngx.noarch.rpm
+yum --enablerepo=remi,remi-php72 install -y nginx php-fpm -y
+yum --enablerepo=remi,remi-php56 install -y php-common php-opcache php-pecl-apcu php-cli php-pear php-pdo php-mysqlnd php-pgsql php-pecl-mongo php-pecl-sqlite php-pecl-memcache php-pecl-memcached php-gd php-mbstring php-mcrypt php-xml php-mysql -y
+#start service
+systemctl stop httpd.service
+systemctl start nginx.service
+systemctl start php-fpm.service
+#change autostart
+systemctl disable httpd.service
+systemctl enable nginx.service
+systemctl enable php-fpm.service
+nano /etc/nginx/conf.d/default.conf
+
+yum install -y mariadb mariadb-server
+systemctl start mariadb.service
+systemctl enable mariadb.service
+/usr/bin/mysql_secure_installation
+
+yum install composer
+
+#beanstalk
+wget http://cbs.centos.org/kojifiles/packages/beanstalkd/1.9/3.el7/x86_64/beanstalkd-1.9-3.el7.x86_64.rpm
+rpm -ivh beanstalkd-1.9-3.el7.x86_64.rpm
+systemctl enable beanstalkd
+systemctl start beanstalkd
+ps aux | grep beanstalkd
+
+#redis
+yum install redis
+systemctl enable redis
+systemctl start redis
+
+#
+yum install libpng-devel
+
+#install npm
+#update mariadb to 10.3 to fix length limit 767
+https://www.digitalocean.com/community/questions/how-do-i-upgrade-from-mariadb-5-5-to-10-1-on-centos7
+
